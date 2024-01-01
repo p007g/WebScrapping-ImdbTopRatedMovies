f8acc067b9f30a309a33bd37d3ad5abe8e361f90
Paras Goyal <146027736+p007g@users.noreply.github.com>
2024-01-02 01:58:33 +0530

    Update scrape.py
    Update the code.
    Get all of the data and print in the terminal--success

--- scrape.py
+++ scrape.py
@@ -14,5 +14,21 @@ try:
     response = requests.get('https://www.imdb.com/chart/top/', headers={'User-Agent': random.choice(user_agents_list)})
     response.raise_for_status()
 
+    data = BeautifulSoup(response.text, 'html.parser')
+
+    movies = data.find('ul', class_="ipc-metadata-list").find_all('li', class_="ipc-metadata-list-summary-item")
+
+    for movie in movies:
+
+        rank = movie.find('h3', class_="ipc-title__text").text.split('. ')[0]
+        name = movie.find('h3', class_="ipc-title__text").text.split('. ')[1]
+
+        year = movie.find('span', class_="cli-title-metadata-item").text
+
+        rating = movie.find('span', class_="ipc-rating-star").text.split('(')[0]
+
+        print(rank, name, year, rating)
+        
+
 except Exception as e:
     print(e)
