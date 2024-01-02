a3dcefac4586056e5140da703f664a3864f7b79a
Paras Goyal <146027736+p007g@users.noreply.github.com>
2024-01-02 18:23:21 +0530

    scrape the data and stored in excel sheet
    Write the whole code.
    Scrape our data from the URL and stored in our excel sheet.
    Update the Readme file.

--- README.md
+++ README.md
@@ -31,4 +31,42 @@ All the data contain inside the HTML tags of the website
     from bs4 import BeautifulSoup  
 
 => now we are going to the google and type 'imdb top rated movies' on search bar--    
-   open the website and copy the URL("https://www.imdb.com/chart/top/").
+   open the website and copy the URL("https://www.imdb.com/chart/top/").  
+
+=> we need to configure our scrapper to **send a fake user-agent with every request**--  
+--to prevent a website to block a web scraper and return a 403 error is because you is telling the website you are a scraper in the user-agents you send to the website when making your requests.  
+for more details : [click here](https://scrapeops.io/web-scraping-playbook/403-forbidden-error-web-scraping/#use-fake-user-agents)
+
+    user_agents_list = [
+    'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+]
+    response = requests.get('https://www.imdb.com/chart/top/', headers={'User-Agent': random.choice(user_agents_list)})
+    response.raise_for_status()
+
+=> two more modules is also required as follows:
+
+    #for fake user agents generation:
+    import random
+
+    #to save the data in excel sheet:use openpyxl
+    import openpyxl
+
+=> and then we scapped our useful data by writing some lines of code:
+
+    data = BeautifulSoup(response.text, 'html.parser')
+
+    movies = data.find('ul', class_="ipc-metadata-list").find_all('li', class_="ipc-metadata-list-summary-item")
+
+    for movie in movies:
+
+        rank = int(movie.find('h3', class_="ipc-title__text").text.split('. ')[0])
+        name = movie.find('h3', class_="ipc-title__text").text.split('. ')[1]
+        year = int(movie.find('span', class_="cli-title-metadata-item").text)
+        rating = float(movie.find('span', class_="ipc-rating-star").text.split('(')[0])
+
+        sheet.append([rank, name, year, rating])
+
+=>  Finally we have the data in Excel Sheet named 'IMDB Movies Rating.xlsx'.
--- scrape.py
+++ scrape.py
@@ -1,10 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import openpyxl
 
+#*****************************************************************************************************************************
+
+#---creating a workbook in excel---
+excel = openpyxl.Workbook()
+sheet = excel.active
+sheet.title = "Top Rated 250 Movies"
+sheet.append(['Movie Rank', 'Movie Name', 'Year of Release', 'IMDB Rating'])
+
+#*****************************************************************************************************************************
 
 try:
 
+    #------getting the Imdb URL request----
     user_agents_list = [
     'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
@@ -14,21 +25,27 @@ try:
     response = requests.get('https://www.imdb.com/chart/top/', headers={'User-Agent': random.choice(user_agents_list)})
     response.raise_for_status()
 
+    #---extracting whole data of URL in HTML format---
     data = BeautifulSoup(response.text, 'html.parser')
 
+    #---find the tag contains our movies list and stored in 'movies'---
     movies = data.find('ul', class_="ipc-metadata-list").find_all('li', class_="ipc-metadata-list-summary-item")
 
+    #---start a loop to find the info/column data we want, 
+       #the info we want to scarpe contains in different tags with its class name provided--- 
     for movie in movies:
 
-        rank = movie.find('h3', class_="ipc-title__text").text.split('. ')[0]
+        rank = int(movie.find('h3', class_="ipc-title__text").text.split('. ')[0])
         name = movie.find('h3', class_="ipc-title__text").text.split('. ')[1]
+        year = int(movie.find('span', class_="cli-title-metadata-item").text)
+        rating = float(movie.find('span', class_="ipc-rating-star").text.split('(')[0])
 
-        year = movie.find('span', class_="cli-title-metadata-item").text
-
-        rating = movie.find('span', class_="ipc-rating-star").text.split('(')[0]
-
-        print(rank, name, year, rating)
-        
+        sheet.append([rank, name, year, rating])
 
 except Exception as e:
     print(e)
+
+#******************************************************************************************************************************
+
+#--save the excel file--
+excel.save('IMDB Movies Rating.xlsx')
